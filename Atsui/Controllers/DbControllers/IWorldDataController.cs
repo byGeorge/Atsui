5f42daec7b64021ab28d33d136ea914815ed4f5c
ByGeorge <User@GORGTOP>
2023-12-17 17:36:50 -0800

    IWorldDataController created, preparing for iitem and swimmer creation and test code

---
+++ Atsui/Controllers/DbControllers/IWorldDataController.cs
@@ -0,0 +1,17 @@
+﻿using Atsui.Models;
+using Atsui.Models.Resources;
+using Humanizer.Localisation;
+
+namespace Atsui.Controllers.DbControllers
+{
+    public interface IWorldDataController
+    {
+        public List<IResource> GetItems(int worldID);
+        public bool CreateItem(IItem item, int worldID);
+        public bool UpdateItem(int itemID, int worldID);
+        public bool DeleteItem(int itemID);
+        public List<Swimmer> GetSwimmers(int worldID);
+        public bool AddSwimmer(int swimmerID, int worldID);
+        public bool DeleteSwimmer(int swimmerID, int itemID);
+    }
+}
--- Atsui/Models/World.cs
+++ Atsui/Models/World.cs
@@ -1,10 +1,21 @@
-﻿namespace Atsui.Models
+﻿using Atsui.Models.Resources;
+
+namespace Atsui.Models
 {
     public class World
     {
-        public World()
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ID { get; }
+        public List<Swimmer> Swimmers { get; set; }
+        public List<IItem> Items { get; set; }
+        public World(string name, string description, int id, List<Swimmer> swimmers, List<IItem> items)
         {
-            
+            Name = name;
+            Description = description;
+            ID = id;
+            Swimmers = swimmers;
+            Items = items;
         }
     }
 }
